9a3b03163d82a268aaf578b28d59e3070b0ba2e6
Faizy-Dot <160147462+Faizy-Dot@users.noreply.github.com>
2024-09-05 00:07:19 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,9 @@ function Header() {
             <a className="mr-5 hover:text-gray-900">Third Link</a>
             <a className="mr-5 hover:text-gray-900">Fourth Link</a>
           </nav>
+          <a href="/products">
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
+            GO
             <svg
               fill="none"
               stroke="currentColor"
@@ -37,6 +38,7 @@ function Header() {
               <path d="M5 12h14M12 5l7 7-7 7" />
             </svg>
           </button>
+          </a>
         </div>
       </header>
     );
